f0d9a20f49fcb64f05f573f86cbeaa9a7f1cd17f
jaquawn <jd23@hawaii.edu>
2018-01-29 10:39:47 -1000

    completes SW2

--- Playground2.playground/Contents.swift
+++ Playground2.playground/Contents.swift
@@ -5,3 +5,29 @@ import UIKit
 var str = "Hello, playground"
 
 
+
+var goodGrades = " i've been doing my work"
+
+if  goodGrades == " i've been doing all my work"
+{
+    print (" i should be getting an A at this point")
+}
+
+// evaluate the conditional to be false
+
+
+if goodGrades == " i'm not doing very well"
+{
+    print ( " i'm doing well in this class")
+}
+
+if goodGrades == " i've been doing all of work"
+{
+    print (" i'm doing well in this class ")
+    
+}
+else
+{
+print (" i'm not doing to well")
+}
+
